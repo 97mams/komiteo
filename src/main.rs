da9ba7fdf614e99a30a86c6793ae3bce86da7fe9
97mams <97mams@gmail.com>
2026-03-02 16:10:40 +0300

    add colored

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::process::Command;
+use std::str;
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", "echo 'hello windows'"])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("git diff")
+            .output()
+            .expect("failed to execute process")
+    };
+
+    if output.status.success() {
+        let stdout = str::from_utf8(&output.stdout).unwrap();
+        println!("Output: {}", stdout);
+    } else {
+        let stderr = str::from_utf8(&output.stderr).unwrap();
+        eprintln!("Error: {}", stderr.on_red());
+    }
 }
