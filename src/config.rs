e260bb89ac98dc6c981d690e49366fad1d4182fd
97mams <97mams@gmail.com>
2026-03-07 14:28:16 +0300

    Update dependencies in Cargo.toml and Cargo.lock; refactor config module for API key handling

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,29 @@
-use serde::{Deserialize, Serialize};
 use std::fs;
-use std::path::PathBuf;
+mod hello;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Config {
-    pub token: String,
+struct Config {
+    api_key: String,
 }
 
-fn config_path() -> PathBuf {
-    dirs::home_dir()
+fn get_api_key() -> Config {
+    let config_path = dirs::home_dir()
         .unwrap()
-        .join("komiteo/config.toml")
-}
+        .join(".komiteo/config.toml");
 
-pub fn load_config() -> Option<Config> {
-    let path = config_path();
-    if path.exists() {
-        let content = fs::read_to_string(path).ok()?;
-        toml::from_str(&content).ok()
-    } else {
-        None
+    if config_path.exists() {
+        let content = fs::read_to_string(config_path).unwrap();
+        return Config {
+            api_key: content.trim().to_string(),
+        };
     }
+
+   let hello = hello::hello_welcome();
+
+    let mut config=  Config {
+        api_key: hello.trim().to_string(),
+    };
+    fs::create_dir_all(config_path.parent().unwrap()).unwrap();
+    fs::write(&config_path, hello.trim()).unwrap();
+
+    config
 }
