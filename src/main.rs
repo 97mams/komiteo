e8218ac05d3e2b87f0390cc2551cb2683fee13d8
97mams <97mams@gmail.com>
2026-03-08 12:10:03 +0300

    feat: retrieve API key and integrate AI agent response in main function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod hello;
 mod config;
 mod cil;
+mod ai;
 fn main() {
-    config::get_api_key();
+     let api_key = config::get_api_key();
     let cmd = cil::cil();
-    println!("Git diff executed successfully {:?}", cmd);
+    let ai_agent_response = ai::ai_agent(&api_key.api_key, &cmd);
+    println!("OpenRouter response: {:?}", ai_agent_response);
 }
