44d05fb452c087aad0ab1e71ca378513a94343ec
97mams <97mams@gmail.com>
2026-03-12 12:36:28 +0300

    feat: implement async main function with reqwest for API calls and response handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+
 mod hello;
 mod config;
 mod cil;
-mod ai;
-fn main() {
-    let api_key = config::get_api_key();
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RequestBody {
+    pub model: String,
+    pub prompt: String,
+    pub stream: bool,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ResponseBody {
+    response: String,
+}
+
+#[tokio::main]
+async  fn main() -> Result<(), Box<dyn std::error::Error>> {
+    hello::hello_welcome();
     let cmd = cil::cil();
-    let content = ai::ai_agent(&api_key.api_key, &cmd);
-    println!("{:?}", content.unwrap());
+
+    let client = Client::new();
+
+    let body = RequestBody {
+        model: "qwen3:8b".to_string(),
+        prompt: cmd,
+        stream: false,
+    };
+
+    let res = client
+        .post("http://localhost:8080/generate")
+        .json(&body)
+        .send()
+        .await?;
+
+    let response_body = res.json::<ResponseBody>().await?;
+    println!("{:?}", response_body.response);
+
+    Ok(())
 }
