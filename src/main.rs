30f4bffd8e5308e9715d4c35afa76f1d8141f839
97mams <97mams@gmail.com>
2026-03-17 18:41:47 +0300

    refactor: update dependencies and restructure main function for clarity

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,77 @@
-use futures_util::StreamExt;
-use openrouter_rs::{OpenRouterClient, api::chat::*, types::Role};
-use crossterm::{
-    event::{self, Event, KeyCode, KeyEventKind},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::io;
+// use futures_util::StreamExt;
+// use openrouter_rs::{OpenRouterClient, api::chat::*, types::Role};
+// use crossterm::{
+//     event::{self, Event, KeyCode, KeyEventKind},
+//     execute,
+//     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+// };
+// use std::io;
 
-mod hello;
-mod config;
-mod cil;
+// mod hello;
+// mod config;
+// mod cil;
+
+use color_eyre::eyre::{Ok,Result};
+use ratatui::{DefaultTerminal,Frame};
 
 
-#[tokio::main]
-async  fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = config::get_api_key().api_key;
-    let key:&str = &config;
-    let diff = cil::diff();
-let client = OpenRouterClient::builder()
-    .api_key(key)
-    .build()?;
+// #[tokio::main]
+fn main() -> Result<()> {
+    // let config = config::get_api_key().api_key;
+    // let key:&str = &config;
+    // let diff = cil::diff();
+    // let client = OpenRouterClient::builder()
+    //     .api_key(key)
+    //     .build()?;
 
-let request = ChatCompletionRequest::builder()
-    .model("arcee-ai/trinity-large-preview:free")
-    .messages(vec![Message::new(Role::User, format!("Generate a short Git commit message in English based on this git diff.
+    // let request = ChatCompletionRequest::builder()
+    //     .model("arcee-ai/trinity-large-preview:free")
+    //     .messages(vec![Message::new(Role::User, format!("Generate a short Git commit message in English based on this git diff.
 
-Format:
-<type>: <body>
+    // Format:
+    // <type>: <body>
 
-Rules:
-- Use conventional types (feat, fix, refactor, chore, docs, test)
-- Present tense
-- One line only
+    // Rules:
+    // - Use conventional types (feat, fix, refactor, chore, docs, test)
+    // - Present tense
+    // - One line only
 
-Git diff:
-{}", diff))])
-    .build()?;
+    // Git diff:
+    // {}", diff))])
+    //     .build()?;
 
-let mut stream = client.chat().stream(&request).await?;
+    // let mut stream = client.chat().stream(&request).await?;
 
-let mut commit_message = String::new();
+    // let mut commit_message = String::new();
 
-while let Some(result) = stream.next().await {
-    if let Ok(response) = result {
-        if let Some(content) = response.choices[0].content() {
-            commit_message.push_str(content);
-            print!("{}", content);
-        }
-    }
-}
-cil::commit(&commit_message);
-cil::push();
-println!();
-loop {
-    // Affichage
-    terminal.draw(|f| ui(f, &input_text))?;
+    // while let Some(result) = stream.next().await {
+    //     if let Ok(response) = result {
+    //         if let Some(content) = response.choices[0].content() {
+    //             commit_message.push_str(content);
+    //             print!("{}", content);
+    //         }
+    //     }
+    // }
+    // cil::commit(&commit_message);
+    // cil::push();
+    // println!();
+    // Ok(())
+    color_eyre::install()?;
 
-    // Gestion des événements externalisée
-    if handle_events(&mut input_text)? {
-        break;
-    }
-}
+    ratatui::run(app)?;
 
     Ok(())
 }
 
-
-fn handle_events(input: &mut String) -> io::Result<bool> {
-    if event::poll(std::time::Duration::from_millis(50))? {
-        if let Event::Key(key) = event::read()? {
-            // Sous Windows, évite le double déclenchement
-            if key.kind == KeyEventKind::Press {
-                match key.code {
-                    KeyCode::Esc => return Ok(true), // On veut quitter
-                    KeyCode::Char(c) => input.push(c),
-                    KeyCode::Backspace => { input.pop(); }
-                    _ => {}
-                }
-            }
+fn app(terminal: &mut DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
         }
     }
-    Ok(false) // On continue
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
