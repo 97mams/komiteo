cf48573b38010dc884a6b6e317da26881a270258
97mams <97mams@gmail.com>
2026-03-28 08:12:34 +0300

    feat: importt hello components

--- src/components/home.rs
+++ src/components/home.rs
@@ -2,7 +2,7 @@ use ratatui::{prelude::*, widgets::*};
 use tokio::sync::mpsc::UnboundedSender;
 
 use super::Component;
-use crate::{action::Action, config::Config};
+use crate::{action::Action, config::Config, _components::hello};
 
 #[derive(Default)]
 pub struct Home {
@@ -40,22 +40,7 @@ impl Component for Home {
     }
 
     fn draw(&mut self, frame: &mut Frame, area: Rect) -> color_eyre::Result<()> {
-        let container = Layout::default().direction(Direction::Vertical).constraints([Constraint::Percentage(80), Constraint::Percentage(20)]).split(frame.area());
-
-        let title =  "Bienvenue sur Komiteo !".light_yellow().bold();
-        let content = Text::from(vec![
-            Line::from(""),
-            Line::from("💫 Le CLI qui automatise votre flux Git avec l'intelligence d'OpenRouter.").alignment(Alignment::Center),
-             Line::from(""),
-            Line::from("👋 Bonjour ! Bienvenue dans l'aventure KOMITEO.").italic(),
-             Line::from(""),
-            Line::from("Avant de pouvoir générer des messages de commit parfaits et de pusher votre code
-en un clin d'œil, nous devons configurer un petit quelque chose.").italic(),
-            Line::from("KOMITEO utilise la puissance de l'IA d'OpenRouter pour comprendre vos changements
-et écrire des messages de commit clairs, concis et standardisés.").italic()
-            ]);
-        let paragraph = Paragraph::new(content).block(Block::default().title(title).border_style(Color::LightYellow ).borders(Borders::ALL)).wrap(Wrap { trim: (true) });
-        frame.render_widget(paragraph, container[0]);
+        let _ = hello::hello(frame);
         Ok(())
     }
 }
