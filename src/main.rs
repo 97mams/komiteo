59c36a5434dad0d07de11591ec01236aaa54919a
97mams <97mams@gmail.com>
2026-04-03 17:22:45 +0300

    Revert "feat: implement rendering of a greeting message in the terminal"

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,86 @@
-use clap::Parser;
-use cli::Cli;
 
-use crate::app::App;
+// use futures_util::StreamExt;
+// use openrouter_rs::{OpenRouterClient, api::chat::*, types::Role};
+// use crossterm::{
+//     event::{self, Event, KeyCode, KeyEventKind},
+//     execute,
+//     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+// };
+// use std::io;
 
-mod action;
-mod app;
-mod cli;
-mod components;
-mod config;
-mod errors;
-mod logging;
-mod tui;
+// mod hello;
+// mod config;
+// mod cil;
 
-#[tokio::main]
-async fn main() -> color_eyre::Result<()> {
-    crate::errors::init()?;
-    crate::logging::init()?;
+use color_eyre::eyre::{Ok,Result};
+use ratatui::{
+    crossterm::{
+        event::{self, Event},
+        terminal,
+    },
+    DefaultTerminal
+};
 
-    let args = Cli::parse();
-    let mut app = App::new(args.tick_rate, args.frame_rate)?;
-    app.run().await?;
-    Ok(())
+
+// #[tokio::main]
+fn main() -> Result<()> {
+    // let config = config::get_api_key().api_key;
+    // let key:&str = &config;
+    // let diff = cil::diff();
+    // let client = OpenRouterClient::builder()
+    //     .api_key(key)
+    //     .build()?;
+
+    // let request = ChatCompletionRequest::builder()
+    //     .model("arcee-ai/trinity-large-preview:free")
+    //     .messages(vec![Message::new(Role::User, format!("Generate a short Git commit message in English based on this git diff.
+
+    // Format:
+    // <type>: <body>
+
+    // Rules:
+    // - Use conventional types (feat, fix, refactor, chore, docs, test)
+    // - Present tense
+    // - One line only
+
+    // Git diff:
+    // {}", diff))])
+    //     .build()?;
+
+    // let mut stream = client.chat().stream(&request).await?;
+
+    // let mut commit_message = String::new();
+
+    // while let Some(result) = stream.next().await {
+    //     if let Ok(response) = result {
+    //         if let Some(content) = response.choices[0].content() {
+    //             commit_message.push_str(content);
+    //             print!("{}", content);
+    //         }
+    //     }
+    // }
+    // cil::commit(&commit_message);
+    // cil::push();
+    // println!();
+    // Ok(())
+    color_eyre::install()?;
+
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+
+    return result;
 }
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Char('q') => break,
+                _ => {
+                }
+            }
+        }
+    }
+    Ok(())
+} 
