195104a0dc1097520c770f04813e49729feee1c9
97mams <97mams@gmail.com>
2026-04-16 19:13:14 +0300

    Add dependencies and implement basic terminal UI with ratatui
    - Updated Cargo.toml to include dependencies: color-eyre, crossterm, and ratatui.
    - Configured release profile for optimizations.
    - Implemented main function to initialize color-eyre and run the ratatui application.
    - Created app function to handle terminal drawing and event reading.
    - Added render function to display a simple "hellow" message in the terminal.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use ratatui::widgets::Paragraph;
+use ratatui::{DefaultTerminal, Frame};
+use ratatui::prelude::*;
 
-fn main() ->iced::Result {
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    ratatui::run(app)?;
+
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    let layout= Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(vec![Constraint::Percentage(90), Constraint::Percentage(10)])
+                .split(frame.area());
+    let title = Text::from("komiteo 0.0.1").fg(Color::LightYellow).bold();
+    let paragraph = Paragraph::new(title);
+    frame.render_widget(paragraph, layout[0]);
 }
