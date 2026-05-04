12ad6586185a6b45cdfd7fa93a67856f6d485231
97mams <97mams@gmail.com>
2026-05-04 13:11:38 +0300

    feat: comment out unused modules and clean up main function

--- src/agent.rs
+++ src/agent.rs
@@ -1,4 +1,4 @@
-use crate::config;
+// use crate::config;
 use crate::cil;
 
 use openrouter_rs::{
@@ -6,8 +6,8 @@ use openrouter_rs::{
     api::chat::*,
     types::{Role},
 };
-use fancy_print::{FancyPrinter, Animation};
-use std::time::Duration;
+// use fancy_print::{FancyPrinter, Animation};
+// use std::time::Duration;
 
 
 
@@ -16,6 +16,7 @@ pub async fn agent() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::get_api_key_from_config().trim().to_string();
     let key:&str = &config;
     let diff = cil::diff();
+
     let client = OpenRouterClient::builder()
         .api_key(key)
         .build()?;
@@ -35,11 +36,10 @@ pub async fn agent() -> Result<(), Box<dyn std::error::Error>> {
     Git diff:
     {}", diff))])
         .build()?;
+
     let response = client.chat().create(&request).await?;
-    
     // cil::commit(&commit_message);
     // cil::push();
-    print!("Press Enter to continue...");
-    println!("{}", response.choices[0].content().unwrap_or("Error: No content in response"));
+    println!("{:?}", response.choices);
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
-mod hello;
-mod cmd;
+use openrouter_rs::{
+    OpenRouterClient,
+    api::chat::*,
+    types::{Role},
+};
+
+// mod hello;
+// mod cmd;
 mod config;
 mod cil;
-mod agent;
-mod tui;
+// mod agent;
+// mod tui;
 
 
-fn main() {
-   let _= tui::render();
+#[tokio::main]
+async  fn main()  -> Result<(), Box<dyn std::error::Error>> {
+   // let _= tui::render();
+   // let _ = agent::agent();
+   let config = config::get_api_key_from_config().trim().to_string();
+    let key:&str = &config;
+    let diff = cil::diff();
+
+    let client = OpenRouterClient::builder()
+        .api_key(key)
+        .build()?;
+
+    let request = ChatCompletionRequest::builder()
+        .model("openrouter/owl-alpha")
+        .messages(vec![Message::new(Role::User, format!("Generate a short Git commit message in English based on this git diff.
+
+    Format:
+    <type>: <body>
+
+    Rules:
+    - Use conventional types (feat, fix, refactor, chore, docs, test)
+    - Present tense
+    - One line only
+
+    Git diff:
+    {}", diff))])
+        .build()?;
+
+    let response = client.chat().create(&request).await?;
+    // cil::commit(&commit_message);
+    // cil::push();
+    println!("{:?}", response.choices);
+    Ok(())
 }
