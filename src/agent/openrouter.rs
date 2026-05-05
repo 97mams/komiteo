f9ab9119d3b12b6e8c137671f25aa7c0a72dda35
97mams <97mams@gmail.com>
2026-05-05 10:15:29 +0300

    feat: add progress spinner and display commit message for OpenRouter agent

--- src/agent/openrouter.rs
+++ src/agent/openrouter.rs
@@ -1,6 +1,9 @@
+use std::time::Duration;
+
 use crate::config::config;
 use crate::git::cil;
 
+use indicatif::{ProgressBar, ProgressStyle};
 use openrouter_rs::{
     OpenRouterClient,
     api::chat::*,
@@ -9,12 +12,20 @@ use openrouter_rs::{
 // use fancy_print::{FancyPrinter, Animation};
 // use std::time::Duration;
 
-pub async fn agent() -> Result<String, Box<dyn std::error::Error>> {
+pub async fn agent() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::get_api_key_from_config().trim().to_string();
     let key:&str = &config;
     let diff = cil::diff();
 
-    println!("{}", config);
+    let pb = ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(120));
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.blue} {msg}")
+            .unwrap()
+            // Choose the tick strings (the characters that cycle)
+            .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏", " "]),
+    );
+    pb.set_message("En attente...");
 
     let client = OpenRouterClient::builder()
         .api_key(key)
@@ -22,7 +33,7 @@ pub async fn agent() -> Result<String, Box<dyn std::error::Error>> {
 
     if diff.is_empty() {
         println!("");
-        return Ok("".to_string());
+        return Ok(());
     }
 
     let request = ChatCompletionRequest::builder()
@@ -45,6 +56,8 @@ pub async fn agent() -> Result<String, Box<dyn std::error::Error>> {
     let message = response.choices[0].content().unwrap_or("").to_string();
     let commit_message = cil::commit(&message);
     // cil::push();
+    pb.finish_with_message("Terminé!");
+    println!("{}", commit_message);
 
-    Ok(commit_message)
+    Ok(())
 }
