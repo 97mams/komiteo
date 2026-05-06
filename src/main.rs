1bc57ce8f6da841b9b92dac67f9d86ac429ccdcc
97mams <97mams@gmail.com>
2026-05-06 16:45:30 +0300

    feat: add event input handling to main function

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod config;
 mod agent;
 mod git;
 mod views;
+mod event;
 
 use crate::views::tui;
 
 #[tokio::main]
 async fn main() {
-    tui::render().await
+    tui::render().await;
+    event::input();
 }
