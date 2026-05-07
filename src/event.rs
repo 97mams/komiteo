34cf972d91cc990850a9dfb8fd0ae9ba01c99c21
97mams <97mams@gmail.com>
2026-05-07 16:02:06 +0300

    feat: enhance input handling with command parsing and reconfiguration prompt

--- src/event.rs
+++ src/event.rs
@@ -2,11 +2,14 @@
 
 // use colored::Colorize;
 
-// use crate::config::config;
-
+use crate::config::config;
+use crate::utils::Command;
+use crate::command::cmd;
 use rustyline::{DefaultEditor, error::ReadlineError};
 
 pub fn input()->rustyline::Result<()>{
+
+    let defalut_commands = vec!["reconfig", "help", "exit"];
 
     let mut rl = DefaultEditor::new()?;
     loop {
@@ -14,8 +17,30 @@ pub fn input()->rustyline::Result<()>{
     match readline {
         Ok(line) => {
             rl.add_history_entry(line.as_str())?;
-            println!("Vous avez entré: {}", line);
-            // cmd::komiteo_cmd(cmd, arg)
+            let value = line.split_whitespace().collect::<Vec<&str>>();
+            match value[0] {
+                "reconfig" => {
+                    reconfig();
+                },
+                "help" => {
+                    println!("Voici les commandes disponibles :");
+                    for cmd in &defalut_commands {
+                        println!("- {} ", cmd);
+                    }
+                },
+                "exit" => break,
+                _ => {
+                    if config::check_api_key() {
+                        // if true in the config file, then we can use the command
+                        let command = Command::parse_cmd(value);
+                        cmd::komiteo_cmd(command.cmd, command.arg);
+                    } else {
+                        config::save_api_key(line.as_str());
+                    }
+                }
+            }
+
+
         },
         Err(ReadlineError::Interrupted) => break, // Ctrl-C
             Err(ReadlineError::Eof) => break,         // Ctrl-D
@@ -26,4 +51,8 @@ pub fn input()->rustyline::Result<()>{
     }
 }
     Ok(())
+}
+
+fn reconfig() {
+    println!("Veuillez entrer votre nouvelle clé API :");
 }
