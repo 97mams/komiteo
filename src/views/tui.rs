cba950b760d8727841b46a97dd578debfbdfa2e7
97mams <97mams@gmail.com>
2026-05-09 17:26:28 +0300

    feat: reorder hello function calls in render for improved API key handling

--- src/views/tui.rs
+++ src/views/tui.rs
@@ -1,18 +1,21 @@
+
 use crate::views::hello;
 use crate::event;
 // use crate::cmd;
 use crate::agent::openrouter;
 use crate::config::config;
-// use crate::hello;
 
 pub async  fn render() {
     hello::logo();
-    hello::hero();
     if !config::check_api_key() {
+        hello::hero();
         hello::description();
+    } else {
+        if let Err(e) = openrouter::agent().await {
+            let _ = e;
+            hello::display_text_with_typing_effect(" Veuillez vérifier votre clé API et votre connexion internet. \n Rentrez à nouveau votre clé API. \n-taper reconfig puis entrer.\n" );
+        }
     }
-   if let Err(e) = openrouter::agent().await {
-        println!("Erreur: {:?}", e);
-    }
+   
     let _ = event::input();
 }
