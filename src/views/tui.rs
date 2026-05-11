e3c7b372b4abe6b1bc344ad6e0e9fabe8d765ee4
97mams <97mams@gmail.com>
2026-05-11 17:26:21 +0300

    refactor: streamline render_state function and improve error handling messages

--- src/views/tui.rs
+++ src/views/tui.rs
@@ -11,24 +11,28 @@ pub async fn render() {
         hello::hero();
         hello::description();
     } else {
-        let status = cil::check_status();
-        if status.is_empty() {
-            hello::display_text_with_typing_effect(
-                " Le prochain commit peut changer tout le projet…\n continue de coder.\n",
-            );
-            return;
-        }
-        for file in status {
-            let diff = cil::diff(file.clone());
-            if let Err(e) = openrouter::agent(diff, file).await {
-                let _ = e;
-                hello::display_text_with_typing_effect(
-                    " Veuillez vérifier votre clé API et votre connexion internet. \n Rentrez à nouveau votre clé API. \n-taper reconfig puis entrer.\n",
-                );
-                break;
-            }
-        }
+        render_state().await;
     }
 
     let _ = event::input();
+}
+
+
+pub async fn render_state() {
+    let status = cil::check_status();
+    if status.is_empty() {
+        hello::display_text_with_typing_effect(
+            " Le prochain commit peut changer tout le projet…\n continue de coder.\n",
+        );
+    }
+    for file in status {
+        let diff = cil::diff(file.clone());
+        if let Err(e) = openrouter::agent(diff, file).await {
+            let _ = e;
+            hello::display_text_with_typing_effect(
+                " Veuillez vérifier votre clé API et votre connexion internet. \n Rentrez à nouveau votre clé API. \n-taper reconfig puis entrer.\n",
+            );
+            break;
+        }
+    }
 }
