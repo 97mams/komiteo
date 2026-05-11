f227de7a0b97af4079e0b7c568ebb2d80d37f2bd
97mams <97mams@gmail.com>
2026-05-11 16:51:20 +0300

    refactor: reorganize imports and improve error handling in render function

--- src/views/tui.rs
+++ src/views/tui.rs
@@ -1,21 +1,34 @@
-
-use crate::views::hello;
 use crate::event;
+use crate::views::hello;
 // use crate::cmd;
 use crate::agent::openrouter;
 use crate::config::config;
+use crate::git::cil;
 
-pub async  fn render() {
+pub async fn render() {
     hello::logo();
     if !config::check_api_key() {
         hello::hero();
         hello::description();
     } else {
-        if let Err(e) = openrouter::agent().await {
-            let _ = e;
-            hello::display_text_with_typing_effect(" Veuillez vérifier votre clé API et votre connexion internet. \n Rentrez à nouveau votre clé API. \n-taper reconfig puis entrer.\n" );
+        let status = cil::check_status();
+        if status.is_empty() {
+            hello::display_text_with_typing_effect(
+                " Le prochain commit peut changer tout le projet…\n continue de coder.\n",
+            );
+            return;
+        }
+        for file in status {
+            let diff = cil::diff(file.clone());
+            if let Err(e) = openrouter::agent(diff, file).await {
+                let _ = e;
+                hello::display_text_with_typing_effect(
+                    " Veuillez vérifier votre clé API et votre connexion internet. \n Rentrez à nouveau votre clé API. \n-taper reconfig puis entrer.\n",
+                );
+                break;
+            }
         }
     }
-   
+
     let _ = event::input();
 }
