30075ec7e4cdc367ac95f50d06f5693abba2694c
97mams <97mams@gmail.com>
2026-05-11 16:51:35 +0300

    refactor: update agent function to accept parameters and improve progress message

--- src/agent/openrouter.rs
+++ src/agent/openrouter.rs
@@ -1,33 +1,26 @@
 use std::time::Duration;
 
 use crate::config::config;
-use crate::git::cil;
 use crate::views::hello;
 
+use crossterm::style::Stylize;
 use indicatif::{ProgressBar, ProgressStyle};
 use openrouter_rs::{
     OpenRouterClient,
     api::chat::*,
     types::{Role},
 };
-// use fancy_print::{FancyPrinter, Animation};
-// use std::time::Duration;
 
-pub async fn agent() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn agent(diff: String, file:String) -> Result<(), Box<dyn std::error::Error>> {
     let config = config::get_api_key_from_config().trim().to_string();
     let key:&str = &config;
-    let diff = cil::diff();
-    if diff.is_empty() {
-        hello::display_text_with_typing_effect(" Le prochain commit peut changer tout le projet…\n continue de coder.\n");
-        return Ok(());
-    }
     let pb = ProgressBar::new_spinner();
     pb.enable_steady_tick(Duration::from_millis(120));
     pb.set_style(
         ProgressStyle::with_template("{spinner:.green} {msg}")
             .unwrap()
             // Choose the tick strings (the characters that cycle)
-            .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏", " "]),
+            .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏", "-"]),
     );
     pb.set_message("En attente...");
 
@@ -57,7 +50,7 @@ pub async fn agent() -> Result<(), Box<dyn std::error::Error>> {
     let message = response.choices[0].content().unwrap_or("").to_string();
     // let commit_message = cil::commit(&message);
     // cil::push();
-    pb.finish_with_message("Commit terminé!");
+    pb.finish_with_message(file.green().to_string());
 
 
     hello::display_text_with_typing_effect(format!(" - {} \n", &message).as_mut_str());
