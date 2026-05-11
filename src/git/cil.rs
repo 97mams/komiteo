edfd799902833ef6a1db4e41459fde71d00452ac
97mams <97mams@gmail.com>
2026-05-11 16:51:43 +0300

    refactor: update diff function to accept file name and add check_status function for git status

--- src/git/cil.rs
+++ src/git/cil.rs
@@ -1,8 +1,23 @@
 use std::process::Command;
+
+use crate::utils::clean_file_name;
  
-pub fn diff() -> String {
+pub fn check_status() -> Vec<String> {
+  let cmd = Command::new("git")
+    .args(&["status", "--porcelain"])
+    .output()
+    .expect("Failed to execute git status");
+
+  let response =String::from_utf8_lossy(&cmd.stdout).to_string();
+
+  response.lines().map(|s| s.to_string()).collect()
+ }
+
+pub fn diff(file_name: String) -> String {
+  let file = clean_file_name(file_name);
   let cmd = Command::new("git")
     .arg("diff")
+    .arg(file)
     .output()
     .expect("Failed to execute git diff");
 
