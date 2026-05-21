ab4f8261e029ceb140dc97a6d8a6ed4b10dfecc5
97mams <97mams@gmail.com>
2026-05-21 16:11:49 +0300

    refactor: simplify code structure and remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,27 @@ mod git;
 mod utils;
 mod views;
 
+use tokio::time::{
+    timeout,
+    Duration,
+};
+
 use crate::views::tui;
 
 #[tokio::main]
 async fn main() {
     tui::render().await;
     let _ = event::input().await;
+
+    let duration = Duration::from_secs(2);
+
+    match timeout(duration, r()).await {
+        Ok(r) => println!("Result: {:?}", r),
+        Err(_) => println!("Operation timed out after {} seconds.", duration.as_secs()),
+    }
+    
+}
+
+async fn r () -> Result<String, ()> {
+    Ok("Hello, world!".to_string())
 }
