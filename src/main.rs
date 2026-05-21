5bc4018abd382b71c9ccdf5488b1a8b38f2b0704
97mams <97mams@gmail.com>
2026-05-21 16:33:33 +0300

    chore: update dependencies and configuration files

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,11 @@ mod git;
 mod utils;
 mod views;
 
-use tokio::time::{
-    timeout,
-    Duration,
-};
-
 use crate::views::tui;
 
 #[tokio::main]
 async fn main() {
     tui::render().await;
-
-    let duration = Duration::from_secs(2);
-
-    match timeout(duration, r()).await {
-        Ok(r) => println!("Result: {:?}", r),
-        Err(_) => println!("Operation timed out after {} seconds.", duration.as_secs()),
-    }
     let _ = event::input().await;
     
-}
-
-async fn r () -> Result<String, ()> {
-    Ok("Hello, world!".to_string())
 }
--- src/views/tui.rs
+++ src/views/tui.rs
@@ -1,8 +1,5 @@
 use colored::Colorize;
-use tokio::time::{
-    timeout,
-    Duration,
-};
+use tokio::time::{interval, Duration};
 
 use crate::views::hello::{self, display_text_with_typing_effect};
 use crate::agent::openrouter;
@@ -19,16 +16,12 @@ pub async fn render() {
         println!(" Tapez {} si vous avez besoin d'aide.."," help ".on_bright_yellow());
     }
 
-    let duration = Duration::from_secs(2);
-
-    match timeout(duration, r()).await {
-        Ok(r) => println!("Result: {:?}", r),
-        Err(_) => println!("Operation timed out after {} seconds.", duration.as_secs()),
+    let mut interval = interval(Duration::from_secs(1));
+    loop {
+        interval.tick().await;
+        println!(" Vérification des changements toutes les 1 secondes... ");
     }
-}
 
-async fn r  () -> Result<String, ()> {
-    Ok("Hello, world!".to_string())
 }
 
 pub async fn render_state() {
