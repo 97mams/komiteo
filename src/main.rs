faec67d7e5b5134c4e41a235c03dd960bd1b0bfd
97mams <97mams@gmail.com>
2026-05-31 19:58:31 +0300

    feat(git): log git check_log output in main function

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ mod args;
 
 use clap::Parser;
 
-use crate::views::tui;
+use crate::{git::cil, views::tui};
 
 #[tokio::main]
 async fn main() { 
+    println!("log {}", cil::check_log());
     let _ = args::Komiteo::parse();
     tui::render().await;
     let _ = event::input().await;
